ef524e48f4e8d3b0240ce0a2472a41e48ce585d2
Levi Saturnino <levisaturnino@gmail.com>
2021-11-29 13:23:28 -0300

    classe application 100% de cobertura

--- src/test/java/br/com/levi/apiteste/ApiTestApplicationTests.java
+++ src/test/java/br/com/levi/apiteste/ApiTestApplicationTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiTesteApplicationTests {
+class ApiTestApplicationTests {
 
     @Test
-    void contextLoads() {
+    void main() {
+        ApiTesteApplication.main(new String[]{});
     }
 
 }
