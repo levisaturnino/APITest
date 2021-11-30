f1ec3363ae893d9f36ef9e8d2a84b7845b3d10fb
Levi Saturnino <levisaturnino@gmail.com>
2021-11-30 00:29:54 -0300

    finalizado as coberturas

--- src/main/java/br/com/levi/apiteste/domian/dtos/UserDTO.java
+++ src/main/java/br/com/levi/apiteste/domian/dtos/UserDTO.java
@@ -10,6 +10,7 @@ import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
 
     private Integer id;
--- src/test/java/br/com/levi/apiteste/ApiTesteApplicationTests.java
+++ src/test/java/br/com/levi/apiteste/ApiTesteApplicationTests.java
@@ -1,4 +1,3 @@
-/*
 package br.com.levi.apiteste;
 
 import org.junit.jupiter.api.Test;
@@ -13,4 +12,3 @@ class ApiTesteApplicationTests {
     }
 
 }
-*/
